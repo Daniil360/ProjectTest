76a464d63eed76d204c51ee831493ee787135278
Daniil <daniil424342@gmail.com>
2023-03-22 16:57:04 +0600

    new stile

---
+++ main.js
@@ -0,0 +1,16 @@
+// const hamburger = 5;
+// const fries = null;
+
+// if (hamburger && fries) {
+//     console.log("Я сыт");
+// }
+
+
+const hamburger = 5;
+const fries = null;
+
+if(hamburger && fries) {
+    console.log('Я сыт!');
+}
+
+console.log((hamburger && fries)); 
