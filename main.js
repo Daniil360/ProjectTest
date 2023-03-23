62a2872aca90f1f0834d9a93845bf928a095c99a
Daniil <daniil424342@gmail.com>
2023-03-23 14:17:16 +0600

    new day

--- main.js
+++ main.js
@@ -6,11 +6,24 @@
 // }
 
 
-const hamburger = 5;
-const fries = null;
+const hamburger = 3;
+const fries = 3;
+const cola = 0;
+const nuggets = 2;
 
-if(hamburger && fries) {
-    console.log('Я сыт!');
+//мы хотим сказать что у нас должно быть или 3 ровно гамбургера (и) 2 штуки колы
+//дальше мы говорим что после первой комбинации мы хотим проверить еще одну комбинацию
+//потом мы ставим ||(или) и говорим что у нас должно быть либо 3 порции Фри и хотя бы 1 нагетсы
+//или выполнится 1 часть кода hamburger === 3 && cola === 2  ||(или) вторая fries === 3 && nuggets
+
+if(hamburger === 3 && cola === 2 || fries === 3 && nuggets) {
+    console.log('Все сыты!');
+} else {
+    console.log('Мы уходим');
 }
 
-console.log((hamburger && fries)); 
+console.log(hamburger === 3 && cola === 2 || fries === 3 && nuggets)
+
+
+
+
