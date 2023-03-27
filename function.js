a57d0938300463361fe992ddb9b03b574dc937d1
Daniil <daniil424342@gmail.com>
2023-03-27 23:24:56 +0600

    dssas

--- function.js
+++ function.js
@@ -302,3 +302,54 @@ function doNothing() {};//создаем функцию которая не че
 //И после return не ставьте перенос строки а то будет ошибка 
 
 console.log(doNothing() === undefined);
+
+
+
+// function sayHello(name) {
+//     return `Привет, ${name}!`;
+// }
+
+// console.log(sayHello("Антон"));
+
+
+
+
+
+
+
+
+
+
+
+// function returnNeighboringNumbers(num) {
+//     return [num - 1, num, num + 1];
+// }
+
+// console.log(returnNeighboringNumbers(5));
+
+
+
+
+
+
+
+// function getMathResult(num, times) {
+//     if (typeof(times) !== 'number' || times <= 0) {
+//         return num;
+//     }
+
+//     let str = '';
+
+//     for (let i = 1; i <= times; i++) {
+//         if (i === times) {
+//             str += `${num * i}`; 
+//         } else {
+//             str += `${num * i}---`; 
+//         }
+//     }
+
+
+//     return str;
+// }
+
+// console.log(getMathResult(20, 10));
