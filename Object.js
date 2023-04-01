5213a783a98bc7fb03d07a12f841a5fd397bb490
Daniil <daniil424342@gmail.com>
2023-04-01 18:13:05 +0600

    qwertty

---
+++ Object.js
@@ -0,0 +1,21 @@
+"use srtict";
+
+const options = {
+    name: 'test',
+    width: 1024,
+    height: 1024,
+    colors: {
+        border: 'black',
+        bd: 'red'
+    }
+};
+
+// console.log(options.name);
+
+// delete options.name;
+
+// console.log(options); 
+
+for (let key in options) {
+    console.log(`Свойство ${key} имеет значение ${options[key]}`);
+}
--- callback.js
+++ callback.js
@@ -1,4 +1,9 @@
 "use strict";
+//callback - функция которая выполнена после того как другая функция 
+//завершила свое выполнение.
+//callback функция позволяет нам быть уверенными в том что код не 
+//начнет исполнение до того момента пока другой код не завершит свое 
+//выполнение .
 
 function first() {
     setTimeout(function() {
@@ -12,3 +17,19 @@ function second() {
 
 first();
 second();
+
+
+
+
+function learnJS(lang, callback) {
+    console.log(`Я учу: ${lang}`);
+    callback();
+}
+
+function done() {
+    console.log('Я прошел этот урок!');
+}
+
+
+
+learnJS('JavaScript', done);
