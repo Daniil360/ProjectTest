c07494a2b1cc09c7946105f560cee50fd0992944
Daniil <daniil424342@gmail.com>
2023-04-02 22:00:31 +0600

    rettt

--- Object.js
+++ Object.js
@@ -7,15 +7,35 @@ const options = {
     colors: {
         border: 'black',
         bd: 'red'
+    },
+    makeTest: function() {
+        console.log('Test');
     }
 };
 
-// console.log(options.name);
+options.makeTest();
 
+const {border, bg} = options.colors;//Это и есть диструктуризация
+console.log(border);
+
+// console.log(options['colors']['border']);
 // delete options.name;
 
 // console.log(options); 
 
-for (let key in options) {
-    console.log(`Свойство ${key} имеет значение ${options[key]}`);
-}
+
+// let counter = 0;
+// for (let key in options) {
+//     if (typeof(options[key]) === 'object') {
+//         for (let i in options[key]) {
+//             console.log(`Свойство ${i} имеет значение ${options[key][i]}`);
+//         }
+//     } else {
+//         console.log(`Свойство ${key} имеет значение ${options[key]}`);
+//         counter++;
+//     }
+    
+// }
+
+// console.log(counter);
+
---
+++ masiv.js
@@ -0,0 +1,9 @@
+"use strict";
+
+const arr = [1, 2, 3, 6, 8];
+
+arr.pop();  // eудаляет последний элемент из массива
+
+arr.push(10);
+
+console.log(arr);
