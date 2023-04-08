f6692ea28e2584fb41a20a06827ac90e9c55546a
Daniil <daniil424342@gmail.com>
2023-04-08 18:41:39 +0600

    qwerrrre

--- masiv.js
+++ masiv.js
@@ -1,6 +1,13 @@
 "use strict";
 
-const arr = [2, 3, 6, 8, 10];
+const arr = [2, 13, 26, 8, 10];
+arr.sort(compareNum);
+
+console.log(arr);
+
+function compareNum(a, b) {
+    return a - b;
+}
 
 // arr[99] = 0;
 // console.log(arr.length);
@@ -13,30 +20,36 @@ const arr = [2, 3, 6, 8, 10];
 //колбэк функция нужна для того чтобы строго выполнится после определенного 
 //количества действий например сначала выполняется forEach а потом уже выполняются 
 //колбэк функцуии
-arr.forEach(function(item, i, arr) {
-    console.log(`${i}; ${item}; внутри массива ${arr}`);
-})
+// arr.forEach(function(item, i, arr) {
+//     console.log(`${i}; ${item}; внутри массива ${arr}`);
+// })
 
 
 
 
 
 
-// arr.pop();  // удаляет последний элемент из массива
+// // arr.pop();  // удаляет последний элемент из массива
 
-// arr.push(10);
+// // arr.push(10);
 
-// console.log(arr);
+// // console.log(arr);
 
-//перебор массива с помощью цикла
-for (let i = 0; i < arr.length; i++) { //цикл будет отрабатывать до тех пор пока не закончатся элементы в внутри массива
-    console.log(arr[i]);
-}
+// //перебор массива с помощью цикла
+// for (let i = 0; i < arr.length; i++) { //цикл будет отрабатывать до тех пор пока не закончатся элементы в внутри массива
+//     console.log(arr[i]);
+// }
 
-//также есать конструкция перебора  ( for of)- метод он работает только 
-//с масиво пододными сущностями 
+// //также есать конструкция перебора  ( for of)- метод он работает только 
+// //с масиво пододными сущностями 
 
 
-for (let value of arr) {
-    console.log(value);
-}
+// for (let value of arr) {
+//     console.log(value);
+// }
+
+
+// const str = prompt("", "");
+// const products = str.split(", ");
+// products.sort();
+// console.log(products.join('; '));
---
+++ spread.js
@@ -0,0 +1,32 @@
+"use strict";
+
+let a = 5,
+    b = a;
+
+b = b + 5;
+
+console.log(b);
+console.log(a);
+
+const obj = {
+    a: 5,
+    b: 1
+};
+
+// const copy1 = obj; //передает ссылку на уже существующий обьект
+
+// copy1.a = 10;
+
+// console.log(copy1);
+// console.log(obj);
+
+function copy(mainObj) {
+    let objCopy = {};
+
+    let key;
+    for (key in mainObj) {
+        objCopy[key] = mainObj[key];
+    }
+
+    return objCopy;
+}
