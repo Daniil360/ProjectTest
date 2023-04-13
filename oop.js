ce7ccab0d4d9a4d8b011f8d73f809018f431e710
Daniil <daniil424342@gmail.com>
2023-04-13 13:28:12 +0600

    ffssss

--- oop.js
+++ oop.js
@@ -1,7 +1,40 @@
-'use strict';
+//Object.create(); для создания прототипных связей
+//Object.setPrototypeOf(); для создания прототипных связей
+//ООп это наука о том как делать правильную архитектуру , так же JS можно назвать прототипно
+//ореинтированым языком это частный случай ООП один из его стилей 
+//В JS все строится на прототипах 
 
 let str = 'some';
 let strObj = new String(str);
 
-console.log(typeof(str));
-console.log(typeof(strObj));
+// console.log(typeof(str));
+// console.log(typeof(strObj));
+
+
+console.dir([1,2,3]);
+
+
+const soldier = {
+    health: 400,
+    armor: 100,
+    sayHello: function() {
+        console.log('Hello');
+    }
+};
+
+
+const john = Object.create(soldier);
+
+
+// const john = {
+//     health: 100
+// };
+
+
+// john.__proto__ = soldier;
+
+// Object.setPrototypeOf(john, soldier);
+
+// console.log(john.armor);
+
+john.sayHello();
