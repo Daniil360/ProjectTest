b8c7af09406205d1f62730f71278bc366168f68a
Daniil <daniil424342@gmail.com>
2023-04-16 15:26:22 +0600

    trrr

--- zamikanya.js
+++ zamikanya.js
@@ -19,17 +19,32 @@ function createCounter() {
     let counter = 0;
 
 
-    const myfunction = function() {
-        counter = counter + 1;
-        return counter;
+    const myfunction = function() {debugger
+        counter = counter + 1;debugger
+        return counter;debugger
     }
 
     return myfunction;
 }
-
-const increment = createCounter();
-const c1 = increment();
-const c2 = increment();
-const c3 = increment();
+debugger
+const increment = createCounter();debugger
+const c1 = increment();debugger
+const c2 = increment();debugger
+const c3 = increment();debugger
 
 console.log(c1, c2, c3);
+
+{
+    let msg = 'Hello';
+    console.log(msg);
+}
+
+
+
+for (let i = 0; i < 9; i++) {
+    for (let j = 0; j < 9; j++) {
+        let num = 3;
+    }
+
+    console.log(num);
+}
