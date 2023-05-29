55c8b2d9a99e0294b0c9fe334180bae5f60ed78f
Daniil <daniil424342@gmail.com>
2023-05-29 20:18:32 +0600

    dfjfjfj

--- osnova.js
+++ osnova.js
@@ -1,26 +1,4 @@
+function sumAB (a,b) {
 
-let x = 2, y = 3;
-
-function plus(x) {
-    return x + 1;
-
+    a + b
 }
-
-console.log(plus(y));
-
-let square = function(x) {
-    return x * x;
-    
-};
-
-console.log(square(plus(y)));
-
-const plus1 = x => x + 1;
-const square1 = x => x * x;
-
-console.log(plus1(y))
-
-console.log(square1(plus(y)))
-
-
-console.log(plus)
