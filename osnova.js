e49237cda78d62ccd2f8ad6befe385704366bf60
Daniil <daniil424342@gmail.com>
2023-05-31 19:56:53 +0600

    hghhg

--- osnova.js
+++ osnova.js
@@ -51,3 +51,44 @@ sum3(z,z*2);
 sum3(2,3,4,5,6);
 sum3(6);
 sum3();
+
+function showSum(elem, x , y) {
+    document.querySelector(elem).textContent = x + y;
+}
+
+function showSum2(elem, x , y) {
+    elem.textContent = x + y;
+}
+
+
+showSum('.out-2', 5 , 6);
+const out1 = document.querySelector('.out-1')
+showSum2(out1, 70 , 6);
+
+
+
+function showSum3() {
+    console.log(arguments);
+    document.querySelector(elem).textContent = arguments[0] + 
+    arguments[1];
+
+}
+
+showSum3(10, 15, ".out-2");
+
+function showSumAll() {
+    console.log(arguments);
+    let sum = 0;
+    for (let i = 0; i < arguments.length; i++) {
+        sum += argumens[i];
+    }
+    console.log(sum);
+}
+
+
+showSumAll(4, 5, 6, '.out-2');
+
+
+function showSumAll() {
+    console.log(arguments);
+}
