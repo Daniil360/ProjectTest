081efa048eb6a919ec2994f15ef0d91906fbee17
Daniil <daniil424342@gmail.com>
2023-05-31 14:42:47 +0600

    fjfj

--- osnova.js
+++ osnova.js
@@ -1,4 +1,18 @@
-function sumAB (a,b) {
+function f8(a) {
+    try {
+        if(a.w==1) {return a.w }
+        return 888
+    } catch (error) {
+        return "Ошибка. Нет А"
+    }
+}
 
-    a + b
+console.log(f8({w:22}))
+
+function S() {
+    return "Автомобиль"
+}
+
+function N() {
+    return 2021
 }
